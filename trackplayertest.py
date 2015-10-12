d37b69e43ffff0719a14816c6439337b1b3a37ac
Brett Henderson <bretth18@gmail.com>
2015-10-11 17:46:37 -0700

    added extra logging
    now saves to a .log file in file path

--- trackplayertest.py
+++ trackplayertest.py
@@ -8,6 +8,21 @@ import logging
 import spotify
 import getpass
 
+#Logger:
+logger = logging.getLogger('scope.name')
+
+file_log_handler = logging.FileHandler('logfile.log')
+logger.addHandler(file_log_handler)
+
+stderr_log_handler = logging.StreamHandler()
+logger.addHandler(stderr_log_handler)
+    #output format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_log_handler.setFormatter(formatter)
+stderr_log_handler.setFormatter(formatter)
+
+logger.info('Info message')
+logger.error('Error message')
 #debuging logger enabled
 #logging.basicConfig(level=logging.DEBUG)
 t = Terminal()
